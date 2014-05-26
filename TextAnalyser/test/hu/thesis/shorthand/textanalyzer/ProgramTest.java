4dfc1a117f9579740b824e604a7ee6f1d542bc2f
hungariandude <hungariandude@users.noreply.github.com>
2014-05-26 10:23:39 +0200

    Updated test text files.

--- TextAnalyser/test/hu/thesis/shorthand/textanalyzer/ProgramTest.java
+++ TextAnalyser/test/hu/thesis/shorthand/textanalyzer/ProgramTest.java
@@ -6,8 +6,9 @@ import hu.thesis.shorthand.textanalyzer.util.ResourceUtils;
 public class ProgramTest {
 
     public static void main(String[] args) {
-	String[] files = { "alkotmany.txt", "egri_csillagok.txt",
-		"ember_tragediaja.txt" };
+	String[] files = { "arany_ember.txt", "egri_csillagok.txt",
+		"ember_tragediaja.txt", "koszivu_ember_fiai.txt",
+		"szent_peter_esernyoje.txt", "legy_jo_mindhalalig.txt" };
 	String[] urls = new String[files.length];
 	for (int i = 0; i < files.length; ++i) {
 	    urls[i] = ResourceUtils.getResourceAsURL(files[i]).getPath();
